16273daa9ed6657120832536d77f8d2e197e7bd2
pchp250g8-home <pchp250g8@tutamail.com>
2025-12-03 11:09:20 +0300

    NumOfDigits

---
+++ NumOfDigits/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input an Integer Number:");
+var strNum = Console.ReadLine();
+Int64.TryParse(strNum, out var iNum);
+var nDigits = 0;var iVal = Math.Abs(iNum);
+while (iVal > 0)
+{
+    iVal /= 10;
+    nDigits++;
+}
+Console.WriteLine("The number {0} has {1} digit(s)", iNum, nDigits);
+Console.Read();
